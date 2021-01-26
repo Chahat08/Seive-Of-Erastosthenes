a5c8fc51906386c25d7d96c0316c9b523de2cb67
Chahat Kalsi <chahat.ck88@gmail.com>
2021-01-26 14:56:01 +0530

    Create intArray.c

---
+++ intArray.c
@@ -0,0 +1,26 @@
+// The Seive of Erastosthenes method of finding primes below N
+// Using an integer array
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10000
+
+int main()
+{
+  int i, j;
+	int a[N];
+
+	for (i = 2; i < N; ++i) a[i] = 1;
+
+	for (i = 2; i < N; ++i)
+	{
+		if(a[i])
+			for (j = i; j <= N / i; ++j)
+				if (a[i * j]) a[i * j] = 0;
+	}
+
+	for (i = 2; i < N; ++i)
+		if (a[i]) printf("%4d\n", i);
+
+	return 0;
+}
